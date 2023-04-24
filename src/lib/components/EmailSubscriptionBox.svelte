abbb2895609ce1d9518a4ca14e8de2962dd9f7b8
jonathannguyen <jonathannguyen@oldrochester.org>
2023-04-24 10:04:58 -0400

    Start email box

---
+++ src/lib/components/EmailSubscriptionBox.svelte
@@ -0,0 +1,4 @@
+<script>
+	import src from '$lib/assets/kobby-mendez-UOl_IN96w0M-unsplash.jpg';
+</script>
+<img src={src} alt="A golden pineapple"/>
--- src/routes/product/+page.svelte
+++ src/routes/product/+page.svelte
@@ -1,1 +1,6 @@
+<script>
+	import EmailSubscriptionBox from '$lib/components/EmailSubscriptionBox.svelte';
+</script>
 <h1>This is your product page</h1>
+
+<EmailSubscriptionBox />
