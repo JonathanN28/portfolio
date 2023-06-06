11cdda08ba3062d0801dc645cae440bbce61fa1b
JonathanN28 <98481216+JonathanN28@users.noreply.github.com>
2023-06-06 12:39:38 -0400

    storefront grid

--- src/lib/components/storefront/ProductGrid.svelte
+++ src/lib/components/storefront/ProductGrid.svelte
@@ -2,16 +2,40 @@
 	import ProductGridCard from "$lib/components/storefront/ProductGridCard.svelte";
 </script>
 
-<div class="grid">
-	<ProductGridCard
-	Title="Minecraft"
-	Price="$29.99"
-	ImageURL="https://images.igdb.com/igdb/image/upload/t_screenshot_big/ar4cx.jpg";
-	/>
+<div class="main flex justify-center">
+	<div class="grid grid-cols-1 md:grid-cols-5 gap-10">
+		<ProductGridCard
+			Title="Minecraft"
+			Price="$29.99"
+			ImageURL="https://images.igdb.com/igdb/image/upload/t_original/sc8d2x.jpg";
+		/>
+		<ProductGridCard
+			Title="Bloons TD 6"
+			Price="$9.99"
+			ImageURL="https://images.igdb.com/igdb/image/upload/t_original/sc5t1n.jpg";
+		/>
+		<ProductGridCard
+			Title="Among Us"
+			Price="$4.99"
+			ImageURL="https://images.igdb.com/igdb/image/upload/t_original/sceeq6.jpg";
+		/>
+		<ProductGridCard
+			Title="Barony"
+			Price="$14.99"
+			ImageURL="https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/clans/9110426/8b3aaa3496ff795fbf52f05f1380eae5937a3b1c.jpg";
+			GIFURL="https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/clans//9110426/a5d219ad9b3e69ca619e2023f8bba6a14fa991a3.gif"
+		/>
+		<ProductGridCard
+			Title="Rust"
+			Price="$39.99"
+			ImageURL="https://images.igdb.com/igdb/image/upload/t_screenshot_big/ar4cx.jpg";
+		/>
+	</div>
 </div>
 
-<style>
-	div {
 
+<style>
+	.main {
+			background: linear-gradient(180deg, #F3F4F5, #235789, #F3F4F5);
 	}
 </style>
--- src/lib/components/storefront/ProductGridCard.svelte
+++ src/lib/components/storefront/ProductGridCard.svelte
@@ -1,21 +1,60 @@
 <script>
+	import RedButton from '$lib/components/storefront/RedButton.svelte';
 	export let Title;
 	export let Price;
 	export let ImageURL;
+	export let GIFURL;
 </script>
-<div class="flex">
-	<img src={ImageURL}
-			 alt="Minecraft Alex, Steve, Pig, Sheep, Wolf, Creeper, Spider, & Zombie on a mountain.">
-	<h2>{Title}</h2>
-	<p>{Price}</p>
-</div>
+<!-- Check the url last three letters -->
+<!-- check if gif url is empty -->
+{#if GIFURL}
+	<div class="card flex" onmouseenter="">
+		<img src={GIFURL}
+				 alt="">
+		<div class="content">
+			<h2>{Title}</h2>
+			<p>{Price}</p>
+		</div>
+	</div>
+{:else}
+<div class="card flex">
 
+	<img src={ImageURL}
+			 alt="">
+	<div class="content">
+		<h2>{Title}</h2>
+		<p>{Price}</p>
+	</div>
+</div>
+{/if}
 <style>
-	div {
+	.card {
 			background-color: blueviolet;
-			@apply w-56 h-56;
+			position: relative;
+			@apply w-72 h-72;
+
+      border: 10px outset #ea3f45;
+			@apply rounded-2xl overflow-hidden;
+	}
+	img {
+			@apply object-cover;
+	}
+	img:hover {
+	}
+	.content {
+			background: linear-gradient(180deg, rgba(243, 244, 245, 0), rgba(243,
+			244, 245, 0.7) 30%, rgba(243, 244, 245) 90%);
+
+			color: #0C1115;
+			position: absolute;
+
+			@apply left-0 right-0 bottom-0;
+			@apply w-full h-auto mx-auto px-4 pb-2 pt-4;
 	}
 	h2 {
 			@apply text-4xl;
+	}
+	p {
+			@apply text-2xl;
 	}
 </style>
